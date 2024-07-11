405fb05497ef86569dc4ad4f62ae29a739e1a322
abde <mahsarabderrahmane@gmail.com>
2024-07-11 16:42:02 +0200

    first commit

---
+++ condicionales.js
@@ -0,0 +1,17 @@
+// console.log('hello')
+let caracter=prompt('entrar un caracter aqui')
+if(caracter==='+'){
+    console.log('sumar')
+}
+else if(caracter==='-'){
+    console.log('restar')
+}
+else if(caracter==='*'){
+    console.log('multiplicar')
+}
+else if(caracter==='/'){
+    console.log('dividir')
+}
+else{
+    console.log('no conozco esa operación')
+}
